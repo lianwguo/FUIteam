210c526105022be7402f8d74a643f99b3c5d2d99
Lian Guo <lianwguo@gmail.com>
2018-08-06 22:23:36 -0400

    fixing site 222 files, adding summary tables

--- scripts/env/8.2.18.DensitySites.R
+++ scripts/env/8.2.18.DensitySites.R
@@ -24,16 +24,39 @@ Index50mi <- which(merc222dist < 80467.2)
 subset25miHg222 <- loc_LAmercA[Index25mi,]
 subset25miHg222
 unique(subset25miHg222$Water.Body.Site)
+write.csv(subset25miHg222, "~/FUIteam/PydioData/env/data_outputs/subset25miHg222.csv")
+unique(subset25miHg222$CollectYear)
+#only collected from these closest sites in in 2007.
+#Test dates are all early in the year (Feb to July)
+
+#trying to get some summary stats - going to just export as csv and look at pivot tables - first change back to dataframe
+CloseMerc222 <- as.data.frame(subset25miHg222)
+head(CloseMerc222)
+str(CloseMerc222)
+class(CloseMerc222)
+write.csv(CloseMerc222, "~/FUIteam/PydioData/env/data_outputs/CloseMerc222.csv")
+
+#table of sampling frequency for each site over 2005-2009 time period
+samplFreq222 <- with(CloseMerc222, table(Water.Body.Site, Collection.Date))
+samplFreq222
+write.csv(samplFreq222, "~/FUIteam/PydioData/env/data_outputs/samplFreq222.csv")
+
+#species frequency for sites per year
+spFreq222 <- with(CloseMerc222, table(Water.Body.Site, Common.Name, CollectYear))
+spFreq222
+write.csv(spFreq222, "~/FUIteam/PydioData/env/data_outputs/spFreq222.csv")
+
 
 #now 50 miles
 subset50miHg222 <- loc_LAmercA[Index50mi,]
 subset50miHg222
 unique(subset50miHg222$Water.Body.Site)
 
+
 #plot - based on files with local projections
 plot(loc_NOLA,
      col = "yellow",
-     main = "Merc Sites within 50 miles of Site 222")
+     main = "Merc Sites within 25 miles of Site 222")
 plot(loc_StateBound,
      add = TRUE)
 plot(subset25miHg222,
@@ -70,9 +93,28 @@ Index50mi306 <- which(merc306dist < 80467.2)
 #subset sites of interest within 25 miles, using vectors of indexes
 subset25miHg306 <- loc_LAmercA[Index25mi306,]
 head(subset25miHg306)
+unique(subset25miHg306$Water.Body.Site)
 unique(subset25miHg306$Collection.Date)
+unique(subset25miHg306$CollectYear)
 #only collected from these closest sites in in 2006, 2007.
 #Test dates are all early in the year (Feb to July)
+
+#trying to get some summary stats - going to just export as csv and look at pivot tables - first change back to dataframe
+CloseMerc306 <- as.data.frame(subset25miHg306)
+head(CloseMerc306)
+str(CloseMerc306)
+class(CloseMerc306)
+write.csv(CloseMerc306, "~/FUIteam/PydioData/env/data_outputs/CloseMerc306.csv")
+
+#table of sampling frequency for each site over 2005-2009 time period
+samplFreq306 <- with(CloseMerc306, table(Water.Body.Site, Collection.Date))
+samplFreq306
+write.csv(samplFreq306, "~/FUIteam/PydioData/env/data_outputs/samplFreq306.csv")
+
+#species frequency for sites per year
+spFreq306 <- with(CloseMerc306, table(Water.Body.Site, Common.Name, CollectYear))
+spFreq306
+write.csv(spFreq306, "~/FUIteam/PydioData/env/data_outputs/spFreq306.csv")
 
 #now 50 miles
 subset50miHg306 <- loc_LAmercA[Index50mi306,]
