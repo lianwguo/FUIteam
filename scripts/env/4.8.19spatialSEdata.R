428a255d49b6ae95cf4613015e258afcadf931b8
Lian Guo <lianwguo@gmail.com>
2019-04-15 17:58:15 -0400

    final compilation

--- scripts/env/4.8.19spatialSEdata.R
+++ scripts/env/4.8.19spatialSEdata.R
@@ -351,6 +351,21 @@ dfLA100mi <- as.data.frame(swLA100mi)
 spLA100mi <- unique(dfLA100mi[c("spName", "mripSite")])
 write.csv(spLA100mi, "~/FUIteam/PydioData/env/data_outputs/spLA100mi.csv")
 
+plot(loc_LA)
+plot(swLA100mi,
+     add = TRUE)
+plot(loc_LAmrip,
+     col = 'red',
+     add = TRUE)
+plot(sub50miLA150,
+     col = 'green',
+     add = TRUE)
+plot(sub100miLA150,
+     col = 'blue',
+     add = TRUE)
+plot(loc_NOLA,
+     add = TRUE)
+
 library(dplyr)
 tibbLA50mi <- spLA50mi %>%
   group_by(mripSite) %>%
@@ -361,3 +376,340 @@ tibbLA100mi <- spLA100mi %>%
 write.csv(tibbLA50mi, "~/FUIteam/PydioData/env/data_outputs/tibbLA50mi.csv")
 write.csv(tibbLA100mi, "~/FUIteam/PydioData/env/data_outputs/tibbLA100mi.csv")
 
+###Florida
+##about 200 samples cannot be included because there is no spatial data for them. 
+FLmercLoc <- read.csv(file.path("~/FUIteam/PydioData/env/raw/", "FLmercLocUp.csv"), 
+                      stringsAsFactors = FALSE)
+FLmerc <- read.csv(file.path("~/FUIteam/PydioData/env/raw/", "FL_mercury.csv"), 
+                   stringsAsFactors = FALSE)
+
+#merge location data together
+FLmercSp <- merge(x = FLmerc, y = FLmercLoc, by.x = "Bottle", by.y = "Bottle", all.x = TRUE)
+head(FLmercSp)
+
+#removing NA values
+FLmercSpB <- FLmercSp[27:859,]
+head(FLmercSpB)
+str(FLmercSpB)
+
+#removing extraneous columns
+FLmercSpC <- FLmercSpB[,c(1:4,9:18,25:32)]
+str(FLmercSpC)
+
+#convert to decimal degrees from degrees minutes
+FLmercSpC$LatDD <- with(FLmercSpC, Lat_Deg + (Lat_Min/60))
+FLmercSpC$LonDD <- with(FLmercSpC, (Lon + (Lon_Min/60))*-1)
+
+#remove all na lat/longs, not assigned by FWC or Lian
+FLmercSpD <- subset(FLmercSpC, (!is.na(FLmercSpC$LatDD)))
+unique(FLmercSpD$LatDD) 
+
+sp_FLmrip <- SpatialPointsDataFrame(FLsites[,3:4], FLsites)
+crs(sp_FLmrip) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+loc_FLmrip <- spTransform(sp_FLmrip, CRS("+proj=tmerc +lat_0=24.33333333333333 +lon_0=-82 +k=0.999941177 +x_0=200000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+crs(loc_FLmrip)
+
+#load MSA for Tampa
+StatArea <- readOGR("~/FUIteam/PydioData/env/raw/cb_2017_us_cbsa_20m", "cb_2017_us_cbsa_20m") ##readOGR(directory,shapefilename)
+head(StatArea)
+
+Tampa <- subset(StatArea, StatArea$GEOID == 45300) #45300. Tampa-St. Petersburg-Clearwater, FL Metro Area
+Tampa
+
+crs(Tampa)
+loc_Tampa <- spTransform(Tampa, CRS("+proj=tmerc +lat_0=24.33333333333333 +lon_0=-82 +k=0.999941177 +x_0=200000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+
+which(US_State_Bound$NAME == "Florida")
+FL_Bound <- US_State_Bound[51,]
+FL_Bound
+
+crs(FL_Bound)
+loc_FLbound <- spTransform(FL_Bound, CRS("+proj=tmerc +lat_0=24.33333333333333 +lon_0=-82 +k=0.999941177 +x_0=200000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"))
+
+#spatialize mercury data
+sp_FLmerc <- SpatialPointsDataFrame(FLmercSpD[,24:23], FLmercSpD)
+crs(sp_FLmerc) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+loc_FLmerc <- spTransform(sp_FLmerc, CRS("+proj=tmerc +lat_0=24.33333333333333 +lon_0=-82 +k=0.999941177 +x_0=200000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs "))
+
+plot(loc_FLbound,
+     main = "FL Mercury Testing Sites")
+plot(loc_FLmerc,
+     add = TRUE)
+plot(loc_FLmrip,
+     add = TRUE)
+
+#ready for distances
+#sites in mrip
+#241 544 615 632 712 742 769 770
+plot(loc_Tampa)
+plot(loc_FLmrip,
+     add = TRUE)
+plot(loc_FLmerc,
+     add = TRUE)
+
+#241
+distFL241 <- spDists(loc_FLmrip[1,], loc_FLmerc, longlat = FALSE) #in m
+distFL241
+
+which(distFL241 < 8046.72) #within 5 miles, 14 obs
+which(distFL241 < 16093.4) #within 10 miles, 71 obs
+which(distFL241 < 40233.6) #within 25 miles #returns  sites, 305+ observations
+which(distFL241 < 80467.2) #within 50 miles #returns  sites,780+ observations
+which(distFL241 < 160934.4) #within 100 miles #returns many observations
+
+Ind50miFL241 <- which(distFL241 < 80467.2) #within 50 miles
+Ind100miFL241 <- which(distFL241 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL241 <- loc_FLmerc[Ind50miFL241,]
+str(sub50miFL241)
+unique(sub50miFL241$LatDD) #10 sites
+unique(sub50miFL241$Year) #2004
+sub50miFL241$mripSite <- 241
+
+sub100miFL241 <- loc_FLmerc[Ind100miFL241,]
+str(sub100miFL241)
+unique(sub100miFL241$LatDD) #59 sites
+unique(sub100miFL241$Year) #2003,2004
+sub100miFL241$mripSite <- 241
+
+plot(loc_FLbound)
+plot(loc_FLmrip,
+     col = 'red',
+     add = TRUE)
+plot(loc_Tampa,
+     add = TRUE)
+plot(loc_FLmerc,
+     col = 'green',
+     add = TRUE)
+plot(sub100miFL544,
+     add = TRUE)
+plot(sub50miFL544,
+     col = 'blue',
+     add = TRUE)
+
+#544
+distFL544 <- spDists(loc_FLmrip[2,], loc_FLmerc, longlat = FALSE) #in m
+distFL544
+
+which(distFL544 < 8046.72) #within 5 miles, 0 obs
+which(distFL544 < 16093.4) #within 10 miles, 5 obs
+which(distFL544 < 40233.6) #within 25 miles #returns  sites, 34 observations
+which(distFL544 < 80467.2) #within 50 miles #returns  sites, 65+ observations
+which(distFL544 < 160934.4) #within 100 miles #returns 190+ observations
+
+Ind50miFL544 <- which(distFL544 < 80467.2) #within 50 miles
+Ind100miFL544 <- which(distFL544 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL544 <- loc_FLmerc[Ind50miFL544,]
+str(sub50miFL544)
+unique(sub50miFL544$LatDD) #10
+unique(sub50miFL544$Year) #2004
+sub50miFL544$mripSite <- 544
+
+sub100miFL544 <- loc_FLmerc[Ind100miFL544,]
+str(sub100miFL544)
+unique(sub100miFL544$LatDD) #67 sites
+unique(sub100miFL544$Year) #2003,2004
+sub100miFL544$mripSite <- 544
+
+#615
+distFL615 <- spDists(loc_FLmrip[3,], loc_FLmerc, longlat = FALSE) #in m
+distFL615
+
+which(distFL615 < 8046.72) #within 5 miles, 26 obs
+which(distFL615 < 16093.4) #within 10 miles, 29 obs
+which(distFL615 < 40233.6) #within 25 miles #returns  sites, 54 observations
+which(distFL615 < 80467.2) #within 50 miles #returns  sites, 65+ observations
+which(distFL615 < 160934.4) #within 100 miles #returns 220+ observations
+
+Ind50miFL615 <- which(distFL615 < 80467.2) #within 50 miles
+Ind100miFL615 <- which(distFL615 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL615 <- loc_FLmerc[Ind50miFL615,]
+str(sub50miFL615)
+unique(sub50miFL615$LatDD) #10
+unique(sub50miFL615$Year) #2004
+sub50miFL615$mripSite <- 615
+
+sub100miFL615 <- loc_FLmerc[Ind100miFL615,]
+str(sub100miFL615)
+unique(sub100miFL615$LatDD) #85 sites
+unique(sub100miFL615$Year) #2003,2004
+sub100miFL615$mripSite <- 615
+
+#632
+distFL632 <- spDists(loc_FLmrip[4,], loc_FLmerc, longlat = FALSE) #in m
+distFL632
+
+which(distFL632 < 8046.72) #within 5 miles, 0
+which(distFL632 < 16093.4) #within 10 miles, 1 obs
+which(distFL632 < 40233.6) #within 25 miles #returns  sites, 54 observations
+which(distFL632 < 80467.2) #within 50 miles #returns  sites, 65+ observations
+which(distFL632 < 160934.4) #within 100 miles #returns 156 observations
+
+Ind50miFL632 <- which(distFL632 < 80467.2) #within 50 miles
+Ind100miFL632 <- which(distFL632 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL632 <- loc_FLmerc[Ind50miFL632,]
+str(sub50miFL632)
+unique(sub50miFL632$LatDD) #10
+unique(sub50miFL632$Year) #2004
+sub50miFL632$mripSite <- 632
+
+sub100miFL632 <- loc_FLmerc[Ind100miFL632,]
+str(sub100miFL632)
+unique(sub100miFL632$LatDD) #52 sites
+unique(sub100miFL632$Year) #2003,2004
+sub100miFL632$mripSite <- 632
+
+#712
+distFL712 <- spDists(loc_FLmrip[5,], loc_FLmerc, longlat = FALSE) #in m
+distFL712
+
+which(distFL712 < 8046.72) #within 5 miles, 20
+which(distFL712 < 16093.4) #within 10 miles, 20
+which(distFL712 < 40233.6) #within 25 miles #returns  sites, 54 observations
+which(distFL712 < 80467.2) #within 50 miles #returns  sites, 65+ observations
+which(distFL712 < 160934.4) #within 100 miles #returns 156 observations
+
+Ind50miFL712 <- which(distFL712 < 80467.2) #within 50 miles
+Ind100miFL712 <- which(distFL712 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL712 <- loc_FLmerc[Ind50miFL712,]
+str(sub50miFL712)
+unique(sub50miFL712$LatDD) #10
+unique(sub50miFL712$Year) #2004
+sub50miFL712$mripSite <- 712
+
+sub100miFL712 <- loc_FLmerc[Ind100miFL712,]
+str(sub100miFL712)
+unique(sub100miFL712$LatDD) #103 sites
+unique(sub100miFL712$Year) #2003,2004
+sub100miFL712$mripSite <- 712
+
+#742
+distFL742 <- spDists(loc_FLmrip[6,], loc_FLmerc, longlat = FALSE) #in m
+distFL742
+
+which(distFL742 < 8046.72) #within 5 miles, 0
+which(distFL742 < 16093.4) #within 10 miles, 20
+which(distFL742 < 40233.6) #within 25 miles #returns  sites, 52 observations
+which(distFL742 < 80467.2) #within 50 miles #returns  sites, 54 observations
+which(distFL742 < 160934.4) #within 100 miles #returns 243 observations
+
+Ind50miFL742 <- which(distFL742 < 80467.2) #within 50 miles
+Ind100miFL742 <- which(distFL742 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL742 <- loc_FLmerc[Ind50miFL742,]
+str(sub50miFL742)
+unique(sub50miFL742$LatDD) #10
+unique(sub50miFL742$Year) #2004
+sub50miFL742$mripSite <- 742
+
+sub100miFL742 <- loc_FLmerc[Ind100miFL742,]
+str(sub100miFL742)
+unique(sub100miFL742$LatDD) #100 sites
+unique(sub100miFL742$Year) #2003,2004
+sub100miFL742$mripSite <- 742
+
+#769
+distFL769 <- spDists(loc_FLmrip[7,], loc_FLmerc, longlat = FALSE) #in m
+distFL769
+
+which(distFL769 < 8046.72) #within 5 miles, 1
+which(distFL769 < 16093.4) #within 10 miles, 2
+which(distFL769 < 40233.6) #within 25 miles #returns  sites, 65+ observations
+which(distFL769 < 80467.2) #within 50 miles #returns  sites, 65+ observations
+which(distFL769 < 160934.4) #within 100 miles #returns 156ish observations
+
+Ind50miFL769 <- which(distFL769 < 80467.2) #within 50 miles
+Ind100miFL769 <- which(distFL769 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL769 <- loc_FLmerc[Ind50miFL769,]
+str(sub50miFL769)
+unique(sub50miFL769$LatDD) #10
+unique(sub50miFL769$Year) #2004
+sub50miFL769$mripSite <- 769
+
+sub100miFL769 <- loc_FLmerc[Ind100miFL769,]
+str(sub100miFL769)
+unique(sub100miFL769$LatDD) #52 sites
+unique(sub100miFL769$Year) #2003,2004
+sub100miFL769$mripSite <- 769
+
+#770
+distFL770 <- spDists(loc_FLmrip[8,], loc_FLmerc, longlat = FALSE) #in m
+distFL770
+
+which(distFL770 < 8046.72) #within 5 miles, 1
+which(distFL770 < 16093.4) #within 10 miles, 2
+which(distFL770 < 40233.6) #within 25 miles #returns  sites, 65+ observations
+which(distFL770 < 80467.2) #within 50 miles #returns  sites, 65+ observations
+which(distFL770 < 160934.4) #within 100 miles #returns 156ish observations
+
+Ind50miFL770 <- which(distFL770 < 80467.2) #within 50 miles
+Ind100miFL770 <- which(distFL770 < 160934.4) #within 100 miles
+
+#subset data using index
+#Every LatDD is a "site" in Florida
+sub50miFL770 <- loc_FLmerc[Ind50miFL770,]
+str(sub50miFL770)
+unique(sub50miFL770$LatDD) #10
+unique(sub50miFL770$Year) #2004
+sub50miFL770$mripSite <- 770
+
+sub100miFL770 <- loc_FLmerc[Ind100miFL770,]
+str(sub100miFL770)
+unique(sub100miFL770$LatDD) #52 sites
+unique(sub100miFL770$Year) #2003,2004
+sub100miFL770$mripSite <- 770
+
+#compile top eight sites
+#compile all of 50 mi data together into one dataframe, same for 100mi
+FL50mi <- rbind(sub50miFL241,sub50miFL544,sub50miFL615,sub50miFL632,
+                sub50miFL712,sub50miFL742,sub50miFL769,sub50miFL770)
+nrow(sub50miFL241)
+nrow(FL50mi)
+unique(FL50mi$Species)
+str(FL50mi)
+
+FL100mi <- rbind(sub100miFL241,sub100miFL544,sub100miFL615,sub100miFL632,
+                sub100miFL712,sub100miFL742,sub100miFL769,sub100miFL770)
+nrow(sub100miFL241)
+nrow(FL100mi)
+unique(FL100mi$Species)
+str(FL100mi)
+
+write.csv(FL50mi, "~/FUIteam/PydioData/env/data_outputs/FL50mi.csv")
+write.csv(FL100mi, "~/FUIteam/PydioData/env/data_outputs/FL100mi.csv")
+
+dfFL50mi <- as.data.frame(FL50mi)
+spFL50mi <- unique(dfFL50mi[c("Species", "mripSite")])
+write.csv(spFL50mi, "~/FUIteam/PydioData/env/data_outputs/spFL50mi.csv")
+
+dfFL100mi <- as.data.frame(FL100mi)
+spFL100mi <- unique(dfFL100mi[c("Species", "mripSite")])
+write.csv(spFL100mi, "~/FUIteam/PydioData/env/data_outputs/spFL100mi.csv")
+
+tibbFL50mi <- spFL50mi %>%
+  group_by(mripSite) %>%
+  summarise(n_distinct(Species))
+tibbFL100mi <- spFL100mi %>%
+  group_by(mripSite) %>%
+  summarise(n_distinct(Species))
+write.csv(tibbFL50mi, "~/FUIteam/PydioData/env/data_outputs/tibbFL50mi.csv")
+write.csv(tibbFL100mi, "~/FUIteam/PydioData/env/data_outputs/tibbFL100mi.csv")
